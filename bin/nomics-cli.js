ba78fb15aa00d26822c1a86807d7905c602c1234
Paul Burkart <paulburkat@protonmail.com>
2020-04-05 04:16:32 -0400

    First Commit

---
+++ bin/nomics-cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const program = require('commander');
+const pkg = require('../package.json');
+
+program
+	.version(pkg.version)
+	.command('key', 'Manage API Key')
+	.command('check', 'Check Coin Info')
+	.parse(process.argv);
+
